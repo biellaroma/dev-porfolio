48e20eab86fff78f532708b0a85ea3a79430d214
Maykon Mendel <maykonmendel.dev@gmail.com>
2022-10-18 21:24:55 -0300

    Adicionando serviço de e-mail

--- src/components/index/index.jsx
+++ src/components/index/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from '../../pages/home';
 import Contact from '../../pages/contact';
+import ErrorPage from '../error';
 
 import { Container } from 'react-materialize';
 
@@ -12,6 +13,7 @@ export default function Index() {
                 <Routes>
                     <Route index path='/' element={<Home />} />
                     <Route path='/contact' element={<Contact />} />
+                    <Route path='*' element={<ErrorPage />} />
                 </Routes>
             </Container>
         </main>
--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
-import { Row, Col, Card, Input, Button } from 'react-materialize';
+import emailjs from '@emailjs/browser';
+import { Row, Col, Card, Input, Button, Icon } from 'react-materialize';
 import UserProfile from '../../components/userProfile';
 
+require('dotenv').config();
+
 export default function Contact() {
+    const serviceId = process.env.REACT_APP_SERVICE_ID;
+    const templateId = process.env.REACT_APP_TEMPLATE_ID;
+    const userId = process.env.REACT_APP_USER_ID;
+
+    function sendEmail(e) {
+        e.preventDefault();
+
+        emailjs.send(
+            serviceId,
+            templateId,
+            e.target,
+            userId,
+        ).then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+    }
+
     return (
         <Row>
             <Col m={3} s={12}>
@@ -12,11 +34,16 @@ export default function Contact() {
                 <h5>Contact</h5>
                 <Card>
                     <Row>
-                        <Input placeholder="Digite um e-mail válido..." type="e-mail" label="E-mail" s={12} />
-                        <Input placeholder="Digite sua mensagem..." label="Message" s={12} />
+                        <Input email id="TextInput-46" label="E-mail" s={12} placeholder="Digite um e-mail válido" icon={<Icon>email</Icon>}/>
+                        <Input placeholder="Digite sua mensagem..." label="Message" s={12} icon={<Icon>message</Icon>}/>
                         <Col s={12} m={12}>
-                            <Button waves="light" className="right grey darken-2">Send</Button>
-                        </Col>
+                            <Button waves="light" floating className="right  green accent-4" title="Enviar" node="button" type="submit" onSubmit={sendEmail}>
+                                <Icon>send</Icon>
+                            </Button>                            
+                            <Button waves="light" floating className="right red accent-4" title="Cancelar" type="reset" style={{ marginRight: '5px' }}>
+                                <Icon>cancel</Icon>
+                            </Button>
+                        </Col>                        
                     </Row>
                 </Card>
             </Col>
